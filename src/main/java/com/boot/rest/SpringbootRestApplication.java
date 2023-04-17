708298995fe294c361d5d5d1cf60bf355de8a39d
jenikinsdocker <jenikinsdocker@gmail.com>
2023-04-17 09:38:14 +0530

    added springboot servlet intilizer

--- src/main/java/com/boot/rest/SpringbootRestApplication.java
+++ src/main/java/com/boot/rest/SpringbootRestApplication.java
@@ -2,9 +2,17 @@ package com.boot.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootRestApplication {
+public class SpringbootRestApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(SpringbootRestApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootRestApplication.class, args);
